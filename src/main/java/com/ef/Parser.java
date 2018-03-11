71bdd9f56c24f9956af37ca78b9a357597bcb62f
iRusev90 <ivan.rusev90@gmail.com>
2018-03-11 18:04:13 +0200

    added stuff

--- src/main/java/com/ef/Parser.java
+++ src/main/java/com/ef/Parser.java
@@ -14,7 +14,7 @@ public class Parser {
 	private LogService logService;
 	
 	public static void main(String[] args) throws IOException {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:/spring.xml");
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:/spring.xml", "classpath:/database.xml");
 		Parser parser = ctx.getBean(Parser.class);
 	
 		parser.run(args);
---
+++ src/main/java/com/ef/entity/Log.java
@@ -0,0 +1,70 @@
+package com.ef.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(schema = "public", name = "logs")
+public class Log {
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	@Column(name = "timestamp", nullable = false, updatable = false)
+	private LocalDateTime timestamp;
+	
+	@Column(name = "ip", nullable = false, updatable = false)
+	private String ip;
+	
+	@Column(name = "request", nullable = false, updatable = false)
+	private String request;
+	
+	@Column(name = "status", nullable = false, updatable = false)
+	private String status;
+	
+	@Column(name = "agent", nullable = false, updatable = false)
+	private String agent;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(LocalDateTime timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	public String getRequest() {
+		return request;
+	}
+	public void setRequest(String request) {
+		this.request = request;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getAgent() {
+		return agent;
+	}
+	public void setAgent(String agent) {
+		this.agent = agent;
+	}
+}
